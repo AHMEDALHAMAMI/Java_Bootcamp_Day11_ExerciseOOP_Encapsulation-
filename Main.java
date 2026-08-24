10a0fc7489f4b6cc3804cb0913589c48d12d3147
AHMEDALHAMAMI <146365804+AHMEDALHAMAMI@users.noreply.github.com>
2026-08-24 12:23:30 +0100

    Add Main class with commented-out code for accounts and employees
    This is a placeholder for future implementation of account and employee functionalities.

---
+++ Main.java
@@ -0,0 +1,49 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Account account1 = new Account("001", "Ahmed", 500000);
+//        Account account2 = new Account();
+//        Account account3 = new Account("003", "Saud", 400000);
+//        Account account4 = new Account("004", "Khalid", 54000);
+//        Account account5 = new Account("005", "Sara", 33000);
+//        account2.setId("002");
+//        account3.setName("Saud");
+//        account4.setBalance(223209);
+//        System.out.println(account3.getBalance());
+//        System.out.println(account3.getName());
+//        System.out.println(account1.getId());
+//        account1.setBalance(20000);
+//        account1.debit(1200);
+//        account1.credit(3600);
+//        System.out.println(account1.getBalance());
+//        System.out.println(account1.toString());
+//        account2.setBalance(3000);
+//        account1.transferTo(account2, 3000);
+//        System.out.println(account2.getBalance());
+//
+//        /////////////////////////////////////////////////////////////////////
+//
+//
+//        Employee employee1 = new Employee("006", "Mishal", 18000);
+//        Employee employee2 = new Employee();
+//        Employee employee3 = new Employee("007", "Abdulrahman", 11570);
+//        Employee employee4 = new Employee();
+//        Employee employee5 = new Employee();
+//
+//        employee2.setName("Tariq");
+//        employee2.setId("009");
+//        employee3.setId("007");
+//        employee4.setSalary(14000);
+//        employee1.setSalary(25000);
+//        System.out.println(employee1.getSalary());
+//        System.out.println(employee1.getAnnualSalary());
+//        employee1.raisedSalary(12);
+//        System.out.println(employee1.raisedSalary(12));
+//        System.out.println(employee1.getName());
+//        System.out.println(employee3.toString());
+//        System.out.println(employee2.getId());
+
+
+    }
+
+}
